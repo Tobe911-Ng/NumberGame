1ae9cd7ef391f1298e505da8eb607f7567ad0a0b
Tobe911-Ng <59968993+Tobe911-Ng@users.noreply.github.com>
2020-08-05 05:04:36 -0700

    Add files via upload

---
+++ NumberGame/Number_Game/Number_Game/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Number_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            int winNum = random.Next(0, 100);
+
+            bool win = false;
+
+            do
+            {
+                Console.Write("Guess a number between 0 and 100: ");
+                string s = Console.ReadLine();
+                int i = int.Parse(s);
+
+                if (i > winNum)
+                {
+                    Console.WriteLine("Too high! Guess lower...");
+                }
+                else if (i < winNum)
+                {
+                    Console.WriteLine("Too low! Guess higher...");
+                }
+                else if (i == winNum)
+                {
+                    Console.WriteLine("You win!");
+                    win = true;
+                }
+            } while (win == false);
+
+            Console.WriteLine();
+            Console.WriteLine("Thank you for playing!");
+            Console.Write("Write any key to finish");
+            Console.ReadKey(true);
+        }
+    }
+}
